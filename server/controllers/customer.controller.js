0a1d7dc96250b118c56e50fa97d6f372192d7175
Your name hereOscarwarmark <oscar.warmark@gmail.com>
2023-09-12 15:53:40 +0200

    register customer to Database

--- server/controllers/customer.controller.js
+++ server/controllers/customer.controller.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const bcrypt = require("bcrypt");
 const { initStripe } = require("../stripe");
 const stripe = initStripe();
 const CustomerDB = "./db/customerDB.json";
@@ -11,9 +12,6 @@ const createCustomer = async (req, res) => {
     email: customerData.email,
   });
 
-  console.log("HÄR!!!", customer);
-  console.log(customerData);
-
   if (customer) {
     const newCustomer = {
       id: customer.id,
@@ -21,6 +19,27 @@ const createCustomer = async (req, res) => {
       email: customer.email,
       password: "",
     };
+
+    const hashedPassword = await bcrypt.hash(customerData.password, 10);
+    newCustomer.password = hashedPassword;
+    console.log(newCustomer);
+
+    fs.readFile(CustomerDB, "utf-8", (err, data) => {
+      if (err) {
+        console.log(err.message);
+      } else {
+        const customers = JSON.parse(data);
+        customers.push(newCustomer);
+
+        fs.writeFile(CustomerDB, JSON.stringify(customers, null, 2), (err) => {
+          if (err) {
+            console.log(err.message);
+          } else {
+            console.log("New customers saved to DB");
+          }
+        });
+      }
+    });
   }
 };
 
